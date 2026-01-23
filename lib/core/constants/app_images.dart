c66f93627495dd14ea357740ebc6785ddf414e83
jShubh-AD <shubhjain798@gmail.com>
2026-01-23 13:41:15 +0530

    added: dummy data for search tab

--- lib/core/constants/app_images.dart
+++ lib/core/constants/app_images.dart
@@ -5,5 +5,11 @@ abstract class AppImages {
   static const String ai1 = "$_baseImage/ai1.png";
   static const String ai2 = "$_baseImage/large height.jpeg";
   static const String appLogo = "$_baseImage/pinterest_logo.png";
+  static const String searchBanner1 = "$_baseImage/search_banner_1.jpeg";
+  static const String searchBanner2 = "$_baseImage/search_banner_2.jpeg";
+  static const String searchBanner3 = "$_baseImage/search_banner_3.jpeg";
+  static const String searchBanner4 = "$_baseImage/search_banner_4.jpeg";
+  static const String searchBanner5 = "$_baseImage/search_banner_5.jpeg";
+  static const String searchBanner6 = "$_baseImage/search_banner_6.jpeg";
 
 }
--- lib/features/search/presentation/views/search.dart
+++ lib/features/search/presentation/views/search.dart
@@ -39,7 +39,7 @@ class _SearchState extends State<Search> {
             CustomRefreshIndicator(
               offsetToArmed: 80,
               onRefresh: () async {
-                await Future.delayed(Duration(seconds: 10));
+                await Future.delayed(Duration(seconds: 2));
               },
               builder: (context, child, controller) {
                 log("ctrl state: ${controller.state}");
@@ -82,17 +82,8 @@ class _SearchState extends State<Search> {
                 child: Column(
                   children: [
                     bannerCarousel(),
-                    ListView.builder(
-                      shrinkWrap: true,
-                      physics: NeverScrollableScrollPhysics(),
-                      itemBuilder: (c, i) => GestureDetector(
-                        onTap: () {
-                          print(" i m taped: $i");
-                        },
-                        child: Text("index : $i"),
-                      ),
-                      itemCount: 50,
-                    )
+                    ideasYouMightLike(false),
+                    ideasYouMightLike(true),
                   ],
                 ),
               ),
@@ -152,12 +143,22 @@ class _SearchState extends State<Search> {
   }
 
   Widget bannerCarousel() {
+
+    final images = [
+      AppImages.searchBanner1,
+      AppImages.searchBanner2,
+      AppImages.searchBanner3,
+      AppImages.searchBanner4,
+      AppImages.searchBanner5,
+      AppImages.searchBanner6
+    ];
+
     return SizedBox(
       height: 390,
       child: Column(
         children: [
           CarouselSlider.builder(
-          itemCount: 2,
+          itemCount: images.length,
           itemBuilder: (_, i, __) => Stack(
             children: [
               SizedBox(
@@ -165,7 +166,7 @@ class _SearchState extends State<Search> {
                 width: double.infinity,
                 child: BuildImage(
                   isNetwork: false,
-                  image: AppImages.ai1,
+                  image: images[i],
                   borderRadius: 0,
                 ),
               ),
@@ -184,20 +185,6 @@ class _SearchState extends State<Search> {
                   ),
                 ),
               ),
-
-              Positioned(
-                  bottom: 16,
-                  left: 12,
-                  right: 0,
-                  child: Text(
-                      "Explore \nAll kinds of AI",
-                      style: GoogleFonts.gelasio(
-                        fontSize: 28,
-                        fontWeight: FontWeight.w500,
-                        color: Colors.white,
-                      )
-                  )
-              )
             ],
           ),
           options: CarouselOptions(
@@ -216,7 +203,7 @@ class _SearchState extends State<Search> {
           Row(
             mainAxisAlignment: MainAxisAlignment.center,
             children: List.generate(
-              2, (i) => AnimatedContainer(
+              images.length, (i) => AnimatedContainer(
               duration: const Duration(milliseconds: 250),
               margin: const EdgeInsets.symmetric(horizontal: 4),
               width:  8,
@@ -233,6 +220,196 @@ class _SearchState extends State<Search> {
 
         ],
       ),
+    );
+  }
+
+  Widget ideasYouMightLike(bool isReverse) {
+    final ideas = [
+      {
+        'image': AppImages.searchBanner1,
+        'title': 'Gaming room inspo',
+        'subtitle': 'Home Decor',
+        'pins': '65 Pins',
+        'time': '1mo',
+        'verified': false,
+      },
+      {
+        'image': AppImages.searchBanner2,
+        'title': 'Fridge door styling',
+        'subtitle': 'Home Decor',
+        'pins': '73 Pins',
+        'time': '2w',
+        'verified': true,
+      },
+      {
+        'image': AppImages.searchBanner3,
+        'title': 'Modern workspace',
+        'subtitle': 'Office Design',
+        'pins': '42 Pins',
+        'time': '3d',
+        'verified': false,
+      },
+    ];
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Padding(
+          padding: const EdgeInsets.fromLTRB(16, 0, 4, 0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                'Explore featured boards',
+                style: GoogleFonts.roboto(
+                  fontSize: 12,
+                  fontWeight: FontWeight.w400,
+                  color: Colors.black87,
+                  letterSpacing: 0.2,
+                ),
+              ),
+              Text(
+                'Ideas you might like',
+                style: GoogleFonts.roboto(
+                  fontSize: 20,
+                  fontWeight: FontWeight.w600,
+                  color: Colors.black,
+                  height: 1.2,
+                ),
+              ),
+            ],
+          ),
+        ),
+        const SizedBox(height: 4),
+        SizedBox(
+          height: 220,
+          child: ListView.builder(
+            scrollDirection: Axis.horizontal,
+            itemCount: ideas.length,
+            reverse: isReverse,
+            itemBuilder: (context, index) {
+              final idea = ideas[index];
+              return Container(
+                width: 220,
+                margin: const EdgeInsets.symmetric(horizontal: 4),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Container(
+                      height: 140,
+                      width: 220,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(16),
+                        color: Colors.grey.shade200,
+                      ),
+                      child: ClipRRect(
+                        borderRadius: BorderRadius.circular(16),
+                        child: GridView.count(
+                          crossAxisCount: 2,
+                          mainAxisSpacing: 2,
+                          crossAxisSpacing: 2,
+                          physics: const NeverScrollableScrollPhysics(),
+                          children: [
+                            BuildImage(
+                              isNetwork: false,
+                              image: idea['image'] as String,
+                              borderRadius: 0,
+                            ),
+                            BuildImage(
+                              isNetwork: false,
+                              image: idea['image'] as String,
+                              borderRadius: 0,
+                            ),
+                            BuildImage(
+                              isNetwork: false,
+                              image: idea['image'] as String,
+                              borderRadius: 0,
+                            ),
+                            BuildImage(
+                              isNetwork: false,
+                              image: idea['image'] as String,
+                              borderRadius: 0,
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                    Padding(
+                        padding: EdgeInsets.symmetric(horizontal: 8),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: [
+                          // Title
+                          Text(
+                            idea['title'] as String,
+                            style: GoogleFonts.roboto(
+                              fontSize: 14,
+                              fontWeight: FontWeight.w600,
+                              color: Colors.black,
+                              height: 1.3,
+                            ),
+                            maxLines: 1,
+                            overflow: TextOverflow.ellipsis,
+                          ),
+                          // Subtitle with verified icon
+                          Row(
+                            children: [
+                              Flexible(
+                                child: Text(
+                                  idea['subtitle'] as String,
+                                  style: GoogleFonts.roboto(
+                                    fontSize: 12,
+                                    fontWeight: FontWeight.w400,
+                                    color: Colors.black87,
+                                  ),
+                                  maxLines: 1,
+                                  overflow: TextOverflow.ellipsis,
+                                ),
+                              ),
+                              if (idea['verified'] as bool) ...[
+                                const SizedBox(width: 4),
+                                const Icon(
+                                  Icons.verified,
+                                  size: 14,
+                                  color: Color(0xFFE60023),
+                                ),
+                              ],
+                            ],
+                          ),
+                          // Pins and time
+                          RichText(
+                            text: TextSpan(
+                              style: GoogleFonts.roboto(
+                                fontSize: 12,
+                                fontWeight: FontWeight.w400,
+                                color: Colors.black54,
+                              ),
+                              children: [
+                                TextSpan(text: idea['pins'] as String),
+                                const TextSpan(text: ' · '),
+                                TextSpan(text: idea['time'] as String),
+                              ],
+                            ),
+                          ),
+                          // "For you" text
+                          Text(
+                            'For you',
+                            style: GoogleFonts.roboto(
+                              fontSize: 11,
+                              fontWeight: FontWeight.w400,
+                              color: Colors.black45,
+                            ),
+                          ),
+                        ],
+                      ),
+                    )
+                  ],
+                ),
+              );
+            },
+          ),
+        ),
+      ],
     );
   }
 }
